b2f76fa99af5e583b59caca3ea71af7e82ae80c3
bbig3831 <beb601@mail.harvard.edu>
2019-12-11 22:04:43 -0500

    Add first set of project files

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET","POST"])
+def index():
+    message = "This is a GET request"
+
+    if request.method == "POST":
+        message = "This is a POST request."
+
+    return render_template('index.html',message=message)
+
+@app.route('/about/<something>')
+def about(something):
+    return f'This page is about {something}.'
